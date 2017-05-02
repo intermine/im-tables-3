70c9dece1885284789085ca697fd9a419611ae4f
Yo Yehudi <yochannah@gmail.com>
2017-05-02 12:50:41 +0100

    default to tsv and add docs to methods

--- src/im_tables/events/exporttable.cljs
+++ src/im_tables/events/exporttable.cljs
@@ -4,24 +4,33 @@
             [clojure.string :refer [join]]
             [im-tables.interceptors :refer [sandbox]]))
 
-(defn encode-file [data filetype]
+;; REMEMBER KIDS, some gene identifiers have a comma in them, because insanity.
+;; This means we default to tsv for Good Reasons.
+
+(def xsv {:csv {:file-type "csv" :separator ","}
+          :tsv {:file-type "tsv" :separator "\t"}})
+
+(defn encode-file
+  "Encode a stringified text file such that it can be downloaded by the browser.
+  Results must be stringified - don't pass objects / vectors / arrays / whatever."
+  [data filetype]
   (ocall js/window "encodeURI" (str "data:text/" filetype ";charset=utf-8," data)))
 
-(defn stringify-query-results [query-results separator]
+(defn stringify-query-results
+  "converts results into a csv/tsv-style string."
+  [query-results separator]
   (reduce (fn [new-str [i rowvals]]
-        (str  new-str
-              (reduce (fn [new-sub-str rowval]
-                        (str new-sub-str separator " " (:value rowval))) "" rowvals)
-              "\n")) "" query-results))
+            (str  new-str
+                  (reduce (fn [new-sub-str rowval]
+                            (str new-sub-str separator " " (:value rowval))) "" rowvals)
+                  "\n")) "" query-results))
 
 (reg-event-fx
  :exporttable/exporttable
  (sandbox)
- (fn [{db :db} [_ _ x]]
+ (fn [{db :db}]
    (let [query-results (get-in db [:query-response :results])
-         stringified-file (stringify-query-results query-results ",")]
-     (ocall js/window "open" (encode-file stringified-file "csv"))
+         file-type (:tsv xsv)
+         stringified-file (stringify-query-results query-results (:separator file-type))]
+     (ocall js/window "open" (encode-file stringified-file (:file-type file-type)))
      {:db db})))
-
-;   :download true
-;   :href (encode-file "kittens, dogs\n1,2" "csv")
