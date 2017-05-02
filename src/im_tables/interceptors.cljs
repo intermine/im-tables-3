99fb6c259713f1c49daa97e29261042d621b99ab
Yo Yehudi <yochannah@gmail.com>
2017-05-02 15:43:06 +0100

    update comments on the sandbox interceptor now that I have seen the light.

--- src/im_tables/interceptors.cljs
+++ src/im_tables/interceptors.cljs
@@ -5,15 +5,13 @@
   "Returns an interceptor factory that shrinks the world
    down to a subset of app-db. Changes are merged back into
    the real db when the handler finishes at a path supplied
-   as the last argument in an event.
+   as the first argument in an event (or second argument if you count the name of the event as an arg).
   (dispatch [:some-event some-data [:my :sandboxed :location])"
   []
   (->interceptor
     :id :sandbox
     :before (fn [context]
               ;(.log js/console "before" context)
-              ;;TODO NOTE: If the second arg in an event dispatch is an HTML element or event target, this method will try to seq over the element and thow an unSeqable error. To workaround, pass it as a second or third arg?
-              ;;further musing: Should the "second" method below be last instead based on the comment at the top? Ask Josh.
               (if-let [path (second (get-in context [:coeffects :event]))]
                 (do
                   ;(println "before found path" path)
