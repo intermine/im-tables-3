d6cfc3a54ad4d8731b2b29b34bec16970f4c3d7c
Joshua Heimbach <joshkh@gmail.com>
2018-01-15 12:44:15 +0000

    Code cleanup

--- src/im_tables/views.cljs
+++ src/im_tables/views.cljs
@@ -15,75 +15,20 @@
        [:button.btn.btn-warning "Modal"]
        [:button.btn.btn-default {:on-click (fn [] (swap! show? not))} "Toggle"]
        (when @show?
-         (into [:div] (map (fn [n]
-                             [main-view/main {:location [:test :location n]
-                                              :service {:root "beta.flymine.org/beta"}
-                                              :settings {:pagination {:limit 10}}
-                                              :query {:from "Gene"
-                                                      :select ["symbol"
-                                                               "secondaryIdentifier"
-                                                               "primaryIdentifier"
-                                                               "organism.name"
-                                                               "dataSets.name"]
-                                                      :where [{:path "Gene.symbol"
-                                                               :op "LIKE"
-                                                               :value "M*"}]}
-                                              #_#_:query {:from "Gene"
-                                                          :select ["symbol"
-                                                                   "secondaryIdentifier"
-                                                                   "primaryIdentifier"
-                                                                   "organism.name"
-                                                                   "publications.firstAuthor"
-                                                                   "dataSets.name"]
-                                                          :joins ["Gene.publications"]
-                                                          :size n
-                                                          :sortOrder [{:path "symbol"
-                                                                       :direction "ASC"}]
-                                                          :where [
-                                                                  {:path "secondaryIdentifier"
-                                                                   :op "="
-                                                                   :value "AC3.1*" ;AC3*
-                                                                   :code "A"}
-                                                                  ]}
-                                              }]
-
-                             ) (range 0 1)))
-         #_[:div.row
-            [:div.col-xs-6 [main-view/main {:location [:test :location]
-                                            :service {:root "beta.flymine.org/beta"}
-                                            ;:query {:from "Gene"
-                                            ;        :select ["Gene.symbol"]
-                                            ;        :where [{:path "Gene.symbol"
-                                            ;                 :op "LIKE"
-                                            ;                 :value "M*"}]}
-                                            :query {:from "Gene"
-                                                    :select ["symbol"
-                                                             "secondaryIdentifier"
-                                                             "primaryIdentifier"
-                                                             "organism.name"
-                                                             "publications.firstAuthor"
-                                                             "dataSets.name"]
-                                                    :joins ["Gene.publications"]
-                                                    :size 5
-                                                    :sortOrder [{:path "symbol"
-                                                                 :direction "ASC"}]
-                                                    :where [
-                                                            {:path "secondaryIdentifier"
-                                                             :op "="
-                                                             :value "AC3.1*" ;AC3*
-                                                             :code "A"}
-                                                            ]}
-                                            }]]
-            #_[:div.col-xs-6 [main-view/main {:location [:test :location2]
-                                              :service {:root "beta.flymine.org/beta"}
-                                              ;:query {:from "Gene"
-                                              ;        :select ["Gene.symbol"]
-                                              ;        :where [{:path "Gene.symbol"
-                                              ;                 :op "LIKE"
-                                              ;                 :value "M*"}]}
-                                              :query {:from "Gene"
-                                                      :select ["Gene.symbol"]
-                                                      :where [{:path "Gene.symbol"
-                                                               :op "="
-                                                               :value "AB*"}]}
-                                              }]]])])))
+         (into [:div]
+               ; Increase these range to produce N number of tables on the same page
+               ; (useful for stress testing)
+               (->> (range 0 30)
+                    (map (fn [n]
+                       [main-view/main {:location [:test :location n]
+                                        :service {:root "beta.flymine.org/beta"}
+                                        :settings {:pagination {:limit 10}}
+                                        :query {:from "Gene"
+                                                :select ["symbol"
+                                                         "secondaryIdentifier"
+                                                         "primaryIdentifier"
+                                                         "organism.name"
+                                                         "dataSets.name"]
+                                                :where [{:path "Gene.symbol"
+                                                         :op "LIKE"
+                                                         :value "M*"}]}}])))))])))
