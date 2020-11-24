07337b0cecc56f27247aa40b7b69e249b22e6908
uosl <regen@anche.no>
2020-11-24 15:37:31 +0000

    Small improvements to dev environment

--- src/im_tables/views.cljs
+++ src/im_tables/views.cljs
@@ -20,6 +20,20 @@
                       :url (fn [{:keys [mine class objectId] :as _vocab}]
                              (string/join "/" [nil mine "report" class objectId]))}}})
 
+(def flymine-config
+  {:service {:root "https://www.flymine.org/flymine"}
+   :query {:from "Gene"
+           :select ["symbol"
+                    "secondaryIdentifier"
+                    "primaryIdentifier"]
+           :where [{:path "Gene"
+                    :op "LOOKUP"
+                    :value "eve"}]}
+   :settings {:pagination {:limit 10}
+              :links {:vocab {:mine "flymine"}
+                      :url (fn [{:keys [mine class objectId] :as _vocab}]
+                             (string/join "/" [nil mine "report" class objectId]))}}})
+
 (def yeastmine-config
   {:service {:root "https://yeastmine.yeastgenome.org/yeastmine"}
    :query {:from "Protein"
@@ -88,12 +102,31 @@
                    {:path "Gene.interactions.participant2"
                     :type "Gene"}]}
    :settings {:pagination {:limit 10}
+              :compact true
+              :links {:vocab {:mine "flymine"}
+                      :url (fn [{:keys [mine class objectId] :as _vocab}]
+                             (string/join "/" [nil mine "report" class objectId]))}}})
+
+(def pickitems-config
+  {:service {:root "https://www.flymine.org/flymine"}
+   :query {:from "Gene"
+           :select ["symbol",
+                    "secondaryIdentifier",
+                    "goAnnotation.ontologyTerm.identifier",
+                    "goAnnotation.ontologyTerm.name",
+                    "goAnnotation.evidence.code.code",
+                    "goAnnotation.ontologyTerm.namespace",
+                    "goAnnotation.qualifier"]
+           :where [{:path "Gene"
+                    :op "LOOKUP"
+                    :value "Notch"}]}
+   :settings {:pagination {:limit 10}
               :links {:vocab {:mine "flymine"}
                       :url (fn [{:keys [mine class objectId] :as _vocab}]
                              (string/join "/" [nil mine "report" class objectId]))}}})
 
 ;; This query has a very wide column, making it great for testing overflow.
-(def flymine-config
+(def flymine-wide-config
   {:service {:root "https://www.flymine.org/flymine"}
    :query {:from "Gene"
            :select ["primaryIdentifier"
@@ -110,6 +143,19 @@
                       :url (fn [{:keys [mine class objectId] :as _vocab}]
                              (string/join "/" [nil mine "report" class objectId]))}}})
 
+(def pagination-bug
+  {:service {:root "https://www.humanmine.org/humanmine"}
+   :query {:from "Gene"
+           :select ["Gene.overlappingFeatures.primaryIdentifier",
+                    "Gene.overlappingFeatures.length",
+                    "Gene.overlappingFeatures.organism.name"]
+           :where [{:path "Gene.id"
+                    :op "="
+                    :value "1016209"}]}
+   :settings {:pagination {:limit 10}
+              :links {:vocab {:mine "humanmine"}
+                      :url (fn [{:keys [mine class objectId] :as _vocab}]
+                             (string/join "/" [nil mine "report" class objectId]))}}})
 
 ; This function is used for testing purposes.
 ; When using im-tables in real life, you could call the view like so:
@@ -140,6 +186,8 @@
             [:button.btn.btn-default {:on-click (fn [] (swap! show? not))}
              (if @show? "Unmount Tables" "Mount Tables")]]]]]]
        (when @show?
-         (into [:div {:style {:max-width "100vw"}}]
+         (into [:div {:style {:max-width "100vw"
+                              :background-color "white"
+                              :border "1px solid #ccc"}}]
                (->> (range 0 number-of-tables)
                     (map (fn [n] [main-view/main [:test :location n]])))))])))
